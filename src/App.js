71dd14bd1758b43906839e0cef1322487f0443e3
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-07 18:05:35 +0530

    made frontpage to add name and contact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  const getContacts = () => {
+    axios.get('http://localhost:5000/contacts')
+      .then(response => {
+        setContacts(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  };
+
+  const handlePhoneChange = event => {
+    setPhone(event.target.value);
+  };
+
+  const handleAddContact = event => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/contacts/add', { name, email, phone })
+      .then(response => {
+        console.log(response);
+        getContacts();
+        setName('');
+        setEmail('');
+        setPhone('');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contacts</h1>
+      <form onSubmit={handleAddContact}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={handleNameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input type="tel" value={phone} onChange={handlePhoneChange} />
+        </label>
+        <br />
+        <button type="submit">Add Contact</button>
+      </form>
+      <h2>All Contacts</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact._id}>
+            {contact.name} - {contact.email} - {contact.phone}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
